2ea318541ec1ba4f4f48c321e3088f1698d5bd8e
Kush Shah <kush@unc.edu>
2023-04-10 13:08:30 -0400

    Import Nebular

--- kushs-org/src/app/app.module.ts
+++ kushs-org/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NbButtonModule, NbLayoutModule, NbSidebarModule, NbThemeModule} from "@nebular/theme";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationComponent
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    RouterModule,
+    NbLayoutModule,
+    NbSidebarModule,
+    NbButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- kushs-org/src/app/home/home.component.ts
+++ kushs-org/src/app/home/home.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `
+<nb-layout>
+  <nb-layout-header fixed>Company Name</nb-layout-header>
+
+  <nb-sidebar>Sidebar Content</nb-sidebar>
+
+  <nb-layout-column>
+    Page Content <button nbButton>Hello World</button>
+  </nb-layout-column>
+</nb-layout>
+`,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
