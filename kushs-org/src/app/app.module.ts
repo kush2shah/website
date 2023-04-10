e5527cd2b55b0a50d60e84938a2708b7cbf90517
Kush Shah <kush@unc.edu>
2023-04-10 13:20:04 -0400

    Revert "Import Nebular"
    This reverts commit 2ea318541ec1ba4f4f48c321e3088f1698d5bd8e.

--- kushs-org/src/app/app.module.ts
+++ kushs-org/src/app/app.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {NbButtonModule, NbLayoutModule, NbSidebarModule, NbThemeModule} from "@nebular/theme";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NbThemeModule.forRoot(),
-    RouterModule,
-    NbLayoutModule,
-    NbSidebarModule,
-    NbButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- kushs-org/src/app/home/home.component.ts
+++ kushs-org/src/app/home/home.component.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-<nb-layout>
-  <nb-layout-header fixed>Company Name</nb-layout-header>
-
-  <nb-sidebar>Sidebar Content</nb-sidebar>
-
-  <nb-layout-column>
-    Page Content <button nbButton>Hello World</button>
-  </nb-layout-column>
-</nb-layout>
-`,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
