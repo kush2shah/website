f83d7af0c25b63ed0ad27c53ab6212cac3028f83
Kush Shah <kush@unc.edu>
2023-10-26 13:34:35 -0400

    Reinstall node modules

--- kushs/src/components/photography/Booking.tsx
+++ kushs/src/components/photography/Booking.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
-import {Button, Checkbox, Form, FormGroup, RadioButton, RadioButtonGroup, TextInput} from "@carbon/react";
+import React, {useEffect, useState} from 'react';
 
 function Booking() {
+
+    const [width, setWidth] = useState(640);
+
+    useEffect(() => {
+        // effect logic
+        const handleResize = () => {
+            if (window.innerWidth < 640) {
+                setWidth(window.innerWidth);
+            } else {
+                setWidth(640);
+            }
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+
+    }, []);
+
+
     return (
         <div>
             <h1>Booking</h1>
@@ -9,8 +30,8 @@ function Booking() {
                 <iframe
 
                     src="https://docs.google.com/forms/d/e/1FAIpQLSdl92VffFUHtbQWkWjmpA0JLg7cUW6TTujHYbf2wcsWS5ZojA/viewform?embedded=true"
-                        width="640"
-                        height="1039"
+                        width={width}
+                        height={width * 1.62}
                         frameBorder="0"
                 >
                     Loading…
