80da8391c0971db5a3bd3f0c7eee54cc3ae9e1bb
Kush Shah <kush@unc.edu>
2023-10-30 00:24:15 -0400

    Change sizing responsiveness and size for mobile

--- kushs/src/components/photography/Booking.tsx
+++ kushs/src/components/photography/Booking.tsx
@@ -4,22 +4,13 @@ function Booking() {
 
     const [width, setWidth] = useState(640);
 
+    function handleResize() {
+        setWidth(window.innerWidth);
+    }
+
     useEffect(() => {
-        // effect logic
-        const handleResize = () => {
-            if (window.innerWidth < 640) {
-                setWidth(window.innerWidth);
-            } else {
-                setWidth(640);
-            }
-        }
-
         window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        }
-
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
 
 
@@ -31,7 +22,7 @@ function Booking() {
 
                     src="https://docs.google.com/forms/d/e/1FAIpQLSdl92VffFUHtbQWkWjmpA0JLg7cUW6TTujHYbf2wcsWS5ZojA/viewform?embedded=true"
                         width={width}
-                        height={width * 1.62}
+                        height={width * 1.6}
                         frameBorder="0"
                 >
                     Loading…
